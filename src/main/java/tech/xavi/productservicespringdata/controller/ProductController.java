e248a53dc6d3f8f91f57b37a30f1d7903e44b9bc
xaviqo <silent90s@gmail.com>
2022-10-12 20:18:08 +0200

    First Commit

---
+++ src/main/java/tech/xavi/productservicespringdata/controller/ProductController.java
@@ -0,0 +1,47 @@
+package tech.xavi.productservicespringdata.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import tech.xavi.productservicespringdata.dto.ProductDto;
+import tech.xavi.productservicespringdata.entity.Product;
+import tech.xavi.productservicespringdata.service.ProductService;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("product")
+public class ProductController {
+
+    private final ProductService service;
+
+    @GetMapping("all")
+    public Flux<ProductDto> all(){
+        return service.getAllProducts();
+    }
+
+    @GetMapping("{id}")
+    public Mono<ResponseEntity<ProductDto>> getById(@PathVariable String id){
+        return service.getProductById(id)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Mono<ProductDto> insert(@RequestBody Mono<ProductDto> productDto){
+        return service.insertProduct(productDto);
+    }
+
+    @PutMapping("{id}")
+    public  Mono<ResponseEntity<ProductDto>> update(@PathVariable String id, @RequestBody Mono<ProductDto> productDto){
+        return service.updateProduct(id,productDto)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("{id}")
+    public Mono<Void> delete(@PathVariable String id){
+        return service.deleteProduct(id);
+    }
+}
---
+++ src/main/java/tech/xavi/productservicespringdata/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package tech.xavi.productservicespringdata.repository;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+import tech.xavi.productservicespringdata.entity.Product;
+
+@Repository
+public interface ProductRepository extends ReactiveMongoRepository<Product,String> {
+
+}
